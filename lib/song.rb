5e638294bf0cf9b0afb352642ac2e81f43a266a6
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-20 20:49:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,39 @@
 class Song
-  attr_accessor :name, :artist
+    attr_accessor :name,:artist
+      @@all = []
+      
+    def initialize(name)
+         @name=name
+         @artist=artist
+         @@all << self
+    end
+   
+    def self.create_by_name(name)
+        song=self.new
+        song.name=name
+        @@all << song
+        song
+    end
+    def self.find_by_name(name)
+      @@all.find {|song|song.name == name}
+    end
+  
+    def self.find_or_create_by_name(name)
+      self.find_by_name(name) || self.create_by_name(name)
+    end
+    def self.new_by_filename(file)
+        song_title=file.chomp(".mp3").split(" - ")
+        song=Song.new(song_title[1])
+        song.artist_name =song_title[0]
+        song
+    end
+    def artist_name=(name)
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)
+    end
 
-  def initialize(name)
-    @name = name
-  end
 
-  def self.new_by_filename(filename)
-    song = filename.split(" - ")[1]
-    artist = filename.split(" - ")[0]
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
-  end
-
-  def artist_name(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
-
+    def self.all
+        @@all
+    end
 end
