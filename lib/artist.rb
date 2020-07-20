1d9088826dbd74fc28c84e734c3d1220e0997547
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-20 20:48:58 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,48 +1,40 @@
 class Artist
-  attr_accessor :name, :songs
-
-  @@all = [ ]
-
+    
+    attr_accessor :name,:songs
+      @@all =[]
+    
   def initialize(name)
-    @name = name
-    @songs = []
+      @name=name
+      @songs =[]
+      @@all << self
+  end
+  def self.all
+      @@all
   end
 
   def add_song(song)
-    self.songs << song
+      song.artist=self
   end
 
-  def save
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+  def songs
+      Song.all.select {|song| song.artist ==self}
   end
 
   def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
+      self.all.detect {|artist| artist.name ==name}
   end
 
   def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
+      artist=Artist.new(name)
+      artist
   end
 
-  def print_songs()
-    @songs.each do |song|
-      puts song.name
-    end
+  def self.find_or_create_by_name(name)
+       self.find(name)  || self.create(name)
   end
+
+  def print_songs
+      self.songs.each {|song| puts song.name}
+  end
+
 end
